7c316034f67a10f077778a2caa7c597004eb7b65
Yu Yamaguchi <yfa57653@gmail.com>
2023-09-06 21:51:38 +0900

    Add Apach license to all files (#10)
    * docs: add Apache License
    
    * docs: add License
    
    * docs: add License

--- heron/models/git_llm/git_mpt/modeling_git_mpt.py
+++ heron/models/git_llm/git_mpt/modeling_git_mpt.py
@@ -1,7 +1,27 @@
-"""PyTorch GIT MPT model.
-This codes is based on modeling_mpt.py in transformers.
-See: https://github.com/huggingface/transformers/blob/main/src/transformers/models/mpt/modeling_mpt.py
+# coding=utf-8
+# Copyright 2023 The HuggingFace Inc. team. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 """
+NOTICE: This code is subject to the terms of the Apache License 2.0.
+
+The code is modified from the original one.
+original code: https://github.com/huggingface/transformers/blob/main/src/transformers/models/mpt/modeling_mpt.py
+
+Additional contributions by Turing Inc. team
+"""
+
 import copy
 from typing import List, Optional, Tuple, Union
 
@@ -9,11 +29,19 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.nn import CrossEntropyLoss
-from transformers import (CLIPVisionConfig, CLIPVisionModel, MptConfig,
-                          MptForCausalLM, MptModel)
+from transformers import (
+    CLIPVisionConfig,
+    CLIPVisionModel,
+    MptConfig,
+    MptForCausalLM,
+    MptModel,
+)
 from transformers.modeling_outputs import (
-    BaseModelOutputWithPast, BaseModelOutputWithPastAndCrossAttentions,
-    BaseModelOutputWithPooling, CausalLMOutputWithPast)
+    BaseModelOutputWithPast,
+    BaseModelOutputWithPastAndCrossAttentions,
+    BaseModelOutputWithPooling,
+    CausalLMOutputWithPast,
+)
 from transformers.models.git.modeling_git import GitProjection
 
 
--- heron/models/utils.py
+++ heron/models/utils.py
@@ -1,3 +1,17 @@
+# Copyright 2023 Turing Inc. Team. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import glob
 from typing import Any, Dict, List, Optional, Tuple
 
@@ -63,7 +77,8 @@ def load_model(
     elif model_type == "git_japanese_stablelm_alpha":
         from .git_llm.git_japanese_stablelm_alpha import (
             GitJapaneseStableLMAlphaConfig,
-            GitJapaneseStableLMAlphaForCausalLM)
+            GitJapaneseStableLMAlphaForCausalLM,
+        )
 
         git_config = GitJapaneseStableLMAlphaConfig.from_pretrained(language_model)
         git_config.set_vision_configs(
@@ -75,8 +90,7 @@ def load_model(
         )
 
     elif model_type == "git_gpt_neox":
-        from .git_llm.git_gpt_neox import (GitGPTNeoXConfig,
-                                           GitGPTNeoXForCausalLM)
+        from .git_llm.git_gpt_neox import GitGPTNeoXConfig, GitGPTNeoXForCausalLM
 
         git_config = GitGPTNeoXConfig.from_pretrained(language_model)
         git_config.set_vision_configs(
