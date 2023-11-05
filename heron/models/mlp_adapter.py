89c8b71b8d6c1b2ee5e511e9ff5c700ad9a5b675
Onely7-nlp <daiki.shiono.s1@dc.tohoku.ac.jp>
2023-11-06 02:46:33 +0900

    modify mlp_adapter.py

--- heron/models/mlp_adapter.py
+++ heron/models/mlp_adapter.py
@@ -8,20 +8,19 @@ class MLPProjection(GitProjection):
         super(MLPProjection, self).__init__(config)
         self.config = config
 
-        if hasattr(config, "mlp_adapter"):
+        if re.match(r'^mlp(\d+)x_gelu$', config.mlp_adapter):
             mlp_gelu_match = re.match(r'^mlp(\d+)x_gelu$', config.mlp_adapter)
-            if mlp_gelu_match:
-                mlp_depth = int(mlp_gelu_match.group(1))
-                modules = [nn.Linear(config.vision_config.hidden_size, config.hidden_size)]
-                for _ in range(1, mlp_depth):
-                    modules.append(nn.GELU())
-                    modules.append(nn.Linear(config.hidden_size, config.hidden_size))
-                modules.append(nn.LayerNorm(config.hidden_size, eps=config.vision_config.layer_norm_eps))
-            else:
-                raise ValueError(f'Unknown mlp_adapter name: {config.mlp_adapter}')
-        else:
+            mlp_depth = int(mlp_gelu_match.group(1))
+            modules = [nn.Linear(config.vision_config.hidden_size, config.hidden_size)]
+            for _ in range(1, mlp_depth):
+                modules.append(nn.GELU())
+                modules.append(nn.Linear(config.hidden_size, config.hidden_size))
+            modules.append(nn.LayerNorm(config.hidden_size, eps=config.vision_config.layer_norm_eps))
+        elif re.match(r'^linear$', config.mlp_adapter):
             modules = [nn.Linear(config.vision_config.hidden_size, config.hidden_size)]
             modules.append(nn.LayerNorm(config.hidden_size, eps=config.vision_config.layer_norm_eps))
+        else:
+            raise ValueError(f'Unknown mlp_adapter name: {config.mlp_adapter}')
 
         self.visual_projection = nn.Sequential(*modules)
 
