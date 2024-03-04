da8a2519c2c45493a241c0f8fd8451ea5a7b7b0f
Ino-Ichan <sys.b11noway@gmail.com>
2024-03-04 01:40:25 +0000

    refactor get_each_dataset function

--- heron/datasets/utils.py
+++ heron/datasets/utils.py
@@ -21,58 +21,31 @@ import yaml
 from torch.utils.data import ConcatDataset, Dataset
 
 from ..models.prepare_processors import get_processor
+from .ja_csv_datasets import JapaneseCSVDataset
+from .ja_csv_instruct_datasets import JapaneseCSVInstructDataset
+from .llava_datasets import LlavaDataset
+from .llava_instruct_datasets import LlavaInstructDataset
+from .m3it_datasets import M3ITDataset
+from .m3it_instruct_datasets import M3ITInstructDataset
+
+dataset_classes = {
+    "m3it": M3ITDataset,
+    "m3it_instruct": M3ITInstructDataset,
+    "japanese_csv": JapaneseCSVDataset,
+    "japanese_csv_instruct": JapaneseCSVInstructDataset,
+    "llava": LlavaDataset,
+    "llava_instruct": LlavaInstructDataset,
+}
 
 
 def get_each_dataset(dataset_config: Dict, processor, max_length: int) -> Tuple[Dataset, Dataset]:
-    if dataset_config["dataset_type"] == "m3it":
-        from .m3it_datasets import M3ITDataset
+    dataset_type = dataset_config["dataset_type"]
+    if dataset_type not in dataset_classes:
+        raise ValueError(f"dataset_type: {dataset_type} is not supported.")
 
-        train_dataset = M3ITDataset.create(dataset_config, processor, max_length, "train")
-        val_dataset = M3ITDataset.create(dataset_config, processor, max_length, "validation")
-
-    elif dataset_config["dataset_type"] == "m3it_instruct":
-        from .m3it_instruct_datasets import M3ITInstructDataset
-
-        train_dataset = M3ITInstructDataset.create(dataset_config, processor, max_length, "train")
-        val_dataset = M3ITInstructDataset.create(
-            dataset_config, processor, max_length, "validation"
-        )
-
-    elif dataset_config["dataset_type"] == "japanese_csv":
-        from .ja_csv_datasets import JapaneseCSVDataset
-
-        train_dataset = JapaneseCSVDataset.create(dataset_config, processor, max_length, "train")
-        val_dataset = JapaneseCSVDataset.create(
-            dataset_config, processor, max_length, "validation"
-        )
-
-    elif dataset_config["dataset_type"] == "japanese_csv_instruct":
-        from .ja_csv_instruct_datasets import JapaneseCSVInstructDataset
-
-        train_dataset = JapaneseCSVInstructDataset.create(
-            dataset_config, processor, max_length, "train"
-        )
-        val_dataset = JapaneseCSVInstructDataset.create(
-            dataset_config, processor, max_length, "validation"
-        )
-
-    elif dataset_config["dataset_type"] == "llava":
-        from .llava_datasets import LlavaDataset
-
-        train_dataset = LlavaDataset.create(dataset_config, processor, max_length, "train")
-        val_dataset = LlavaDataset.create(dataset_config, processor, max_length, "validation")
-
-    elif dataset_config["dataset_type"] == "llava_instruct":
-        from .llava_instruct_datasets import LlavaInstructDataset
-
-        train_dataset = LlavaInstructDataset.create(dataset_config, processor, max_length, "train")
-        val_dataset = LlavaInstructDataset.create(
-            dataset_config, processor, max_length, "validation"
-        )
-
-    else:
-        raise ValueError(f"dataset_type: {dataset_config['dataset_type']} is not supported.")
-
+    DatasetClass = dataset_classes[dataset_type]
+    train_dataset = DatasetClass.create(dataset_config, processor, max_length, "train")
+    val_dataset = DatasetClass.create(dataset_config, processor, max_length, "validation")
     return train_dataset, val_dataset
 
 
