e2308ec131d9c95efc4cc9becd35d6f34b5de255
kentosasaki-jp <s2113605.klis.tsukuba.ac.jp>
2024-03-05 18:32:25 +0900

    chore: update path

--- scripts/llavabench.sh
+++ scripts/llavabench.sh
@@ -4,7 +4,7 @@ export WANDB_PROJECT_NAME="project-name"
 
 PLAYGROUND_PATH=./playground/data/llava-bench-ja
 MODEL_CONFIG=./projects/video_blip/exp001.yml
-OUTPUT_PATH=./output/llava-bench-ja
+OUTPUT_PATH=$PLAYGROUND_PATH/output
 
 # Inference
 python heron/eval/inference_llava_bench.py \
@@ -21,7 +21,7 @@ EXP_NAME=$(basename "${MODEL_CONFIG}" | cut -d'/' -f 5 | cut -d'.' -f 1)
 # Evaluation
 QUESTION_PATH="$PLAYGROUND_PATH/qa90_questions_ja.jsonl"
 CONTEXT_PATH="$PLAYGROUND_PATH/captions_boxes_coco2014_val_80.jsonl"
-ANSWER_LIST_PATHS="$PLAYGROUND_PATH/qa90_gpt4_answers_ja.jsonl $PLAYGROUND_PATH/sample_answers.jsonl"
+ANSWER_LIST_PATHS="$PLAYGROUND_PATH/qa90_gpt4_answers_ja.jsonl $OUTPUT_PATH/${EXP_NAME}_answers.jsonl"
 RULE_PATH="$PLAYGROUND_PATH/rule.json"
 OUTPUT_FILE="${OUTPUT_PATH}/${EXP_NAME}_reviews.json"
 
