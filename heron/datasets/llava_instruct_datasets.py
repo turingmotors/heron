fc872b370e68e0e5ac44af3107f7e060fc05469d
Onely7 <daiki.shiono.s1@dc.tohoku.ac.jp>
2024-05-30 14:52:24 +0000

    modify about llava_instruct_datasets

--- heron/datasets/llava_instruct_datasets.py
+++ heron/datasets/llava_instruct_datasets.py
@@ -62,6 +62,7 @@ class LlavaInstructDataset(BaseDataset):
         dataset_config: Dict,
         processor: HFProcessor,
         max_length: int,
+        model_type: str,
         split: str = "train",
         is_inference: bool = False,
     ):
@@ -96,6 +97,7 @@ class LlavaInstructDataset(BaseDataset):
                 split_datasets["train"],
                 processor,
                 max_length,
+                model_type,
                 is_inference,
                 dataset_config["language"],
                 dataset_config["dataset_root"],
@@ -108,6 +110,7 @@ class LlavaInstructDataset(BaseDataset):
                 split_datasets["test"],
                 processor,
                 max_length,
+                model_type,
                 is_inference,
                 dataset_config["language"],
                 dataset_config["dataset_root"],
@@ -199,29 +202,30 @@ class LlavaInstructDataset(BaseDataset):
         labels = torch.cat(labels_list, dim=-1)
         prompt_attn_mask = torch.cat(attn_mask_list, dim=-1)
 
-        # Add IMAGE_TOKEN(`"<image>"`) token
-        image_token_id = self.processor.tokenizer.convert_tokens_to_ids(IMAGE_TOKEN)
-        image_token_num = (tokenized_prompt == image_token_id).sum().item()
+        if model_type == "llava_llm":
+            # Add IMAGE_TOKEN(`"<image>"`) token
+            image_token_id = self.processor.tokenizer.convert_tokens_to_ids(IMAGE_TOKEN)
+            image_token_num = (tokenized_prompt == image_token_id).sum().item()
 
-        if image_token_num == len(images):
-            pass
-        elif image_token_num < len(images):
-            add_image_token_num = len(images) - image_token_num
-            add_image_token_tensor = torch.full((add_image_token_num,), image_token_id)
-            add_ignore_token_tensor = torch.full((add_image_token_num,), IGNORE_INDEX)
-            add_attn_mask_tensor = torch.full((add_image_token_num,), 1)
-            # update tokenized_prompt, labels, prompt_attn_mask
-            tokenized_prompt_left = tokenized_prompt[:1]
-            tokenized_prompt_right = tokenized_prompt[1:]
-            tokenized_prompt = torch.cat(
-                (tokenized_prompt_left, add_image_token_tensor, tokenized_prompt_right), dim=-1
-            )
-            labels = torch.cat((add_ignore_token_tensor, labels), dim=-1)
-            prompt_attn_mask = torch.cat((add_attn_mask_tensor, prompt_attn_mask), dim=-1)
-        else:
-            raise ValueError(
-                f"The input provided to the model are wrong.\nThe number of image tokens is {image_token_num} while the number of image given to the model is {len(images)}."
-            )
+            if image_token_num == len(images):
+                pass
+            elif image_token_num < len(images):
+                add_image_token_num = len(images) - image_token_num
+                add_image_token_tensor = torch.full((add_image_token_num,), image_token_id)
+                add_ignore_token_tensor = torch.full((add_image_token_num,), IGNORE_INDEX)
+                add_attn_mask_tensor = torch.full((add_image_token_num,), 1)
+                # update tokenized_prompt, labels, prompt_attn_mask
+                tokenized_prompt_left = tokenized_prompt[:1]
+                tokenized_prompt_right = tokenized_prompt[1:]
+                tokenized_prompt = torch.cat(
+                    (tokenized_prompt_left, add_image_token_tensor, tokenized_prompt_right), dim=-1
+                )
+                labels = torch.cat((add_ignore_token_tensor, labels), dim=-1)
+                prompt_attn_mask = torch.cat((add_attn_mask_tensor, prompt_attn_mask), dim=-1)
+            else:
+                raise ValueError(
+                    f"The input provided to the model are wrong.\nThe number of image tokens is {image_token_num} while the number of image given to the model is {len(images)}."
+                )
 
         if len(tokenized_prompt) < self.max_length:
             pad_length = self.max_length - len(tokenized_prompt)
